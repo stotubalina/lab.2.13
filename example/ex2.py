d37f283b8276c5782a05afd10bd2663d47848e2c
stotubalina <totubalina02@bk.ru>
2022-03-31 15:47:27 +0300

    добавлены примеры

---
+++ example/ex2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math as m
+
+if __name__ == '__main__':
+    print(m.sin(m.pi / 7))
---
+++ example/ex5.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from math import *
+if __name__ == '__main__':
+    print(cos(pi))
+    print(sin(pi))
+    print(factorial(2))
